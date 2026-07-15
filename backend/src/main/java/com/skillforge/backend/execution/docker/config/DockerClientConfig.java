bca495a59d09b361d439f6c9bdacb0c51c539fb9
Jashwanth-05 <jashwanthj510@gmail.com>
2026-07-15 10:04:04 +0530

    Add Docker Client Config

---
+++ backend/src/main/java/com/skillforge/backend/execution/docker/config/DockerClientConfig.java
@@ -0,0 +1,27 @@
+package com.skillforge.backend.execution.docker.config;
+
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.core.DefaultDockerClientConfig;
+import com.github.dockerjava.core.DockerClientImpl;
+import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+
+@Configuration
+public class DockerClientConfig {
+
+    @Bean
+    public DockerClient dockerClient(){
+        DefaultDockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder().build();
+        ApacheDockerHttpClient httpClient =
+                new ApacheDockerHttpClient.Builder()
+                        .dockerHost(config.getDockerHost())
+                        .sslConfig(config.getSSLConfig())
+                        .connectionTimeout(Duration.ofSeconds(30))
+                        .responseTimeout(Duration.ofMinutes(2))
+                        .build();
+        return DockerClientImpl.getInstance(config,httpClient);
+    }
+}
